b971b24fabb13ac2a718ffb15c156900cb154923
Seunghyun Hwang <lesomnus@gmail.com>
2024-12-12 18:59:36 +0000

    test with options

--- parser_test.go
+++ parser_test.go
@@ -29,21 +29,22 @@ func TestParser(t *testing.T) {
 
 	test(func(e *Entry) { require.Equal(`"proto3"`, e.Syntax) })
 	test(func(e *Entry) { require.Equal(`example.library`, e.Package) })
+	test(func(e *Entry) { require.Equal(`foo.bar`, e.Option.Name) })
 	test(func(e *Entry) {
 		s := e.Service
 		require.NotNil(s)
 		require.Equal("AuthorService", s.Name)
-		require.Equal(6, s.Pos.Line)
+		require.Equal(14, s.Pos.Line)
 		require.Equal(1, s.Pos.Column)
-		require.Equal(64, s.Pos.Offset)
-		require.Equal(14, s.EndPos.Line) // Where the next entry begin.
+		require.Equal(155, s.Pos.Offset)
+		require.Equal(22, s.EndPos.Line) // Where the next entry begin.
 		require.NotEmpty(s.Entry)
 
 		r := s.Entry[len(s.Entry)-1]
 		require.Equal("Get", r.Method.Name)
-		require.Equal(10, r.Pos.Line)
+		require.Equal(18, r.Pos.Line)
 		require.Equal(2, r.Pos.Column) // There is an indent.
-		require.Equal(217, r.Pos.Offset)
-		require.Equal(260, r.EndPos.Offset) // Where the semicolon is.
+		require.Equal(308, r.Pos.Offset)
+		require.Equal(351, r.EndPos.Offset) // Where the semicolon is.
 	})
 }
